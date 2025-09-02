1a8b3047f9c60e51ea26474f1c8a2421432e49c1
Ruben Malungu <rubenmalungu@googlemail.com>
2025-09-02 10:48:50 +0100

    UTF-8: prevent double-encode and send explicit UTF-8 JSON

---
+++ opti-portfolio.ps1
@@ -0,0 +1,15 @@
+﻿${function:Invoke-SolanaRpc}
+
+${function:Get-ChainPortfolio}
+
+${function:Get-OptiPortfolio}
+
+${function:Get-OptiPortfolioCached}
+
+${function:Get-QuarterInfo}
+
+${function:Show-OptiPortfolio}
+
+${function:Get-AccrualEstimate}
+
+${function:Export-OptiStatement}
