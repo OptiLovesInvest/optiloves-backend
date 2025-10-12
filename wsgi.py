93339b9039f97149b26ca8e99c3eb56a358244cb
Ruben Malungu <rubenmalungu@googlemail.com>
2025-10-12 08:57:10 +0100

    fix(cors): wrap app/application in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,3 +5,14 @@ try:
     app = allow_cors(app)
 except NameError:
     pass
+from cors_mw import allow_cors
+try:
+    application  # gunicorn may use this name
+    application = allow_cors(application)
+except NameError:
+    pass
+try:
+    app
+    app = allow_cors(app)
+except NameError:
+    pass
